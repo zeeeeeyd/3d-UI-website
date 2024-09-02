99fb226661ba29b3eab9238a3f89041f1579e086
Zeyd SAIAD <zeyd.saiad@etu.univ-mosta.dz>
2024-09-03 00:37:40 +0100

    Adding features and how it works components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Highlights from "./components/Highlights"
-import Model from "./components/Model"
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
 
-function App() {
 
+import * as Sentry from '@sentry/react';
+
+const App = () => {
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
       <Model />
+      <Features />
+      <HowItWorks />
     </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
